47712853843b204b934f18fa7e22d7c61f22dee1
DianaPrada <dianitaprada@gmail.com>
2020-09-06 20:06:47 -0500

    Permisos a la cámara
    Se cambiaron los estilos de New gifo, para el video

--- scripts/main_newgifo.js
+++ scripts/main_newgifo.js
@@ -10,6 +10,39 @@ import {changeTheme, verifyTheme} from './darkmode.js';
  * Global variables
  */
 
+const VideoTag = document.getElementById('videoTag');
+
+let constraints = { audio: false, video: { width: 480, height: 320 } }; 
+
+navigator.mediaDevices.getUserMedia(constraints)
+.then(function(mediaStream) {
+  hideDivNewVideoInfo();
+  let video = VideoTag;
+  video.srcObject = mediaStream;
+  video.onloadedmetadata = function(e) {
+    video.play();
+  };
+})
+.catch((error) => {
+    renderMsg(error);
+  });
+
+
+/**
+* @method hideDivNewVideoInfo
+* @description Change class of No Results Div
+* @param {}
+* @returns {}
+*/
+
+const hideDivNewVideoInfo = (() => {
+    const hideNewVideoInfo = document.getElementById('newVideoInfo');
+    hideNewVideoInfo.classList.remove("newVideoInfo");
+    hideNewVideoInfo.classList.add("hidden");
+    VideoTag.classList.remove("hidden");
+    
+    
+  });
 
 
 /**
