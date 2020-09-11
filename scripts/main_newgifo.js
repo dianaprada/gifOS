7d567f3203e33acfb39456fb6e15413fb19b3398
DianaPrada <dianitaprada@gmail.com>
2020-09-11 18:57:35 -0500

    Permisos a la camara
    Permisos a la camara y comportamiento inicial del DIV

--- scripts/main_newgifo.js
+++ scripts/main_newgifo.js
@@ -10,11 +10,31 @@ import {changeTheme, verifyTheme} from './darkmode.js';
  * Global variables
  */
 
+const newVideoInfoConteiner = document.getElementById('newVideoInfo');
 const VideoTag = document.getElementById('videoTag');
+const buttonStart = document.getElementById('buttonStart');
+const buttonRecord = document.getElementById('buttonRecord');
+const buttonFinish = document.getElementById('buttonFinish');
+const ButtonUpload = document.getElementById('ButtonUpload');
 
-let constraints = { audio: false, video: { width: 480, height: 320 } }; 
 
-navigator.mediaDevices.getUserMedia(constraints)
+let constraints = { audio: false, video: { width: 426, height: 240 } }; 
+
+
+
+/**
+ * Listeners
+ */
+
+  /**
+ * @method _startListener
+ * @description: 
+ * @returns {}
+ */
+
+let _startListener = (() => {
+  changeDivNewVideoInfo();
+  navigator.mediaDevices.getUserMedia(constraints)
 .then(function(mediaStream) {
   hideDivNewVideoInfo();
   let video = VideoTag;
@@ -27,6 +47,40 @@ navigator.mediaDevices.getUserMedia(constraints)
     renderMsg(error);
   });
 
+  
+});
+
+  /**
+ * @method addEventListenerButtonStart
+ * @description: 
+ * @returns {}
+ */
+
+const addEventListenerButtonStart = (() => {
+  buttonStart.addEventListener("click", _startListener, false);
+});
+
+
+/**
+ * Show and Hidden
+ */
+
+ /**
+* @method changeDivNewVideoInfo
+* @description Change class of No Results Div
+* @param {}
+* @returns {}
+*/
+
+const changeDivNewVideoInfo = (() => {
+  newVideoInfoConteiner.innerHTML=  `<h3 class="record_video__h3" id="record_video__h3">¿Nos das acceso 
+                                      <br /> a tu cámara?
+                                      <p class="record_video__p" id="record_video__p">El acceso a tu camara será válido sólo
+                                      <br />por el tiempo en el que estés creando el GIFO. </p>`;
+  buttonStart.classList.remove("record__buttons");
+  buttonStart.classList.add("hidden");
+});
+
 
 /**
 * @method hideDivNewVideoInfo
@@ -36,12 +90,11 @@ navigator.mediaDevices.getUserMedia(constraints)
 */
 
 const hideDivNewVideoInfo = (() => {
-    const hideNewVideoInfo = document.getElementById('newVideoInfo');
-    hideNewVideoInfo.classList.remove("newVideoInfo");
-    hideNewVideoInfo.classList.add("hidden");
-    VideoTag.classList.remove("hidden");
-    
-    
+    newVideoInfoConteiner.classList.remove("newVideoInfo");
+    newVideoInfoConteiner.classList.add("hidden");
+    VideoTag.classList.remove("hidden"); 
+    buttonRecord.classList.remove("hidden"); 
+
   });
 
 
@@ -63,6 +116,8 @@ const renderMsg = ((msg) => document.querySelector('.gifos-msg').innerHTML = msg
  * Run
 */
 
+/*  Listeners  */
+addEventListenerButtonStart();
 
 /*  DarkMode  */
 verifyTheme();
